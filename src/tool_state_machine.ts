a65ffbde6d0b4e5bae5170683b033d3403dc99d1
4vanger <github@4vanger.com>
2024-03-28 22:30:27 -0700

    The rest of the state machine

--- src/tool_state_machine.ts
+++ src/tool_state_machine.ts
@@ -9,7 +9,30 @@ export const toolMachine = createMachine({
       on: { turn_on: 'Online' },
     },
     Online: {
-      on: { turn_off: 'Offline' },
+      on: {
+        turn_off: 'Offline',
+        auth: 'Unlocking',
+      },
+    },
+    Unlocking: {
+      on: {
+        success: 'Unlocked',
+        fail: 'Online',
+        turn_off: 'Offline'
+      },
+    },
+    Unlocked: {
+      on: {
+        badge_out: 'Online',
+        usage_started: 'In Use',
+        turn_off: 'Offline'
+      },
+    },
+    "In Use": {
+        on: { usage_stopped: "Unlocked",
+          badge_out: 'Online',
+          turn_off: 'Offline'
+        }
     },
   },
 })
