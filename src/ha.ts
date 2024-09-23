6351dd64e52fbca1d906cc1ace366eb4267c414a
4vanger <github@4vanger.com>
2024-09-22 21:02:08 -0700

    use latest HA URL

--- src/ha.ts
+++ src/ha.ts
@@ -1,4 +1,4 @@
 export function callWebhook(webhookName: WebhookName, params: Record<string, string>) {
   const searchParams = new URLSearchParams(params)
-  return fetch(`http://homeassistant:8123/api/webhook/${webhookName}?${searchParams.toString()}`)
+  return fetch(`http://homeassistant.internal:8123/api/webhook/${webhookName}?${searchParams.toString()}`)
 }
